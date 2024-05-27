946a5d76c0b8d1acfdf2de947d2d386967ad051c
Kamilla Faust <kamilla.cfaust@gmail.com>
2024-05-27 10:38:21 -0300

    ajustando teste e removendo comentários

--- src/test/java/br/com/rocketskills/petlov/Cadastro.java
+++ src/test/java/br/com/rocketskills/petlov/Cadastro.java
@@ -1,43 +1,20 @@
 
 package br.com.rocketskills.petlov;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.time.Duration;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
 
 public class Cadastro {
-
-	// WebDriver driver;
-
-	// @BeforeEach
-	// void start() {
-	//   driver = new ChromeDriver();
-	// 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	// }
-
-	// @AfterEach
-	// void finish() {
-	// 	driver.close();
-	// }
 
 	@Test
 	@DisplayName("Deve poder cadastrar um ponto de doação")
 	void createPoint() {
 
 		open("https://petlov.vercel.app/signup");
+
 		$("h1").shouldHave(text("Cadastro de ponto de doação"));
     $("input[name='name']").setValue("Kamilla Point");
     $("input[name='email']").setValue("kamilla@email.com");
